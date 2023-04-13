852a62f611ecd8d4d4413068d7ff74211bc7bd4b
Nhoj <nhojplummer@yahoo.com>
2023-04-13 12:30:56 -0400

    Input is making it through

--- practice-7/src/components/Form.js
+++ practice-7/src/components/Form.js
@@ -1,7 +1,18 @@
 import React from "react"
 
 function Form() {
-  return <div>Hello World</div>
+  return (
+    <>
+      <form>
+        <div>
+          <input type="text" placeholder="First Name" />
+        </div>
+        <div>
+          <input type="text" placeholder="Last Name" />
+        </div>
+      </form>
+    </>
+  )
 }
 
 export default Form
