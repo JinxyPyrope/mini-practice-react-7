6f234c618ca9e3c8be84963b6ee604d20cbb75dd
Nhoj <nhojplummer@yahoo.com>
2023-04-13 17:42:20 -0400

    Practice is finally complete

--- practice-7/src/components/Form.js
+++ practice-7/src/components/Form.js
@@ -2,23 +2,22 @@ import React, { useState } from "react"
 
 const Form = () => {
   const [firstName, setFirstName] = useState("")
-  // const [lastName, setLastName] = useState("")
-
-  const handleChange = e => {
-    setFirstName(e.target.value)
-  }
+  const [lastName, setLastName] = useState("")
 
   const handleClick = () => {
-    alert("Hello " + firstName)
+    alert("Hello " + firstName + " " + lastName + "!")
     setFirstName("")
-    
+    setLastName("")
   }
 
   return (
     <>
       <form>
         <div>
-          <input type="text" name="firstName" value={firstName} placeholder="First Name" onChange={handleChange} />
+          <input type="text" name="firstName" value={firstName} placeholder="First Name" onChange={e => setFirstName(e.target.value)} />
+        </div>
+        <div>
+          <input type="text" name="lastName" value={lastName} placeholder="Last Name" onChange={e => setLastName(e.target.value)} />
         </div>
       </form>
       <button onClick={handleClick}>GREET ME</button>
